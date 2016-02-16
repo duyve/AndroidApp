f21d431baadb23016c69ccd911c3e2458af3cc7a
WPaul15 <wmpaul@iastate.edu>
2016-02-16 16:43:30 -0600

    Added comments in MainActivity.

--- app/src/main/java/com/example/duyve/myapplication/MainActivity.java
+++ app/src/main/java/com/example/duyve/myapplication/MainActivity.java
@@ -14,6 +14,9 @@ import android.view.MenuItem;
 /* Main Class */
 public class MainActivity extends AppCompatActivity
 {
+    /* This method is called whenever MainActivity is created.
+     * This occurs whenever the app is loaded, MainActivity comes into view, or the orientation
+     * of the screen changes. */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -51,10 +54,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
+        if (id == R.id.action_settings) return true;
 
         return super.onOptionsItemSelected(item);
     }
