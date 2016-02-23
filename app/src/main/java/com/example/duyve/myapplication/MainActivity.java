7ac62a43faf5d34a89b1ee0aafa8341680b016ff
WPaul15 <wmpaul@iastate.edu>
2016-02-22 18:08:59 -0600

    Committing changes in order to pull from master.

--- app/src/main/java/com/example/duyve/myapplication/MainActivity.java
+++ app/src/main/java/com/example/duyve/myapplication/MainActivity.java
@@ -7,21 +7,31 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-/* Main Class */
+/* MainActitvy Class */
 public class MainActivity extends AppCompatActivity
 {
-    /* This method is called whenever MainActivity is created.
-     * This occurs whenever the app is loaded, MainActivity comes into view, or the orientation
-     * of the screen changes. */
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
@@ -41,7 +51,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) return true;
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
