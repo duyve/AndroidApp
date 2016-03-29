6e31de11bd979dfc02c0f95369e6d8710a5c9c4e
WPaul15 <wmpaul@iastate.edu>
2016-03-29 16:17:53 -0500

    Added Internet access requirement for Firebase.

--- app/src/main/java/com/example/duyve/myapplication/EditResumeActivity.java
+++ app/src/main/java/com/example/duyve/myapplication/EditResumeActivity.java
@@ -8,15 +8,17 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class EditResumeActivity extends Activity {
-
+public class EditResumeActivity extends Activity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_resume);
     }
 
-    public void onClick(View view){
+    public void onClick(View view)
+    {
         TextView text = (TextView) findViewById(R.id.textView2);
         String value = text.getText().toString();
         Intent i = new Intent(this, EditHeaderActivity.class);
@@ -25,22 +27,28 @@ public class EditResumeActivity extends Activity {
     }
 
     @Override
-    public void onActivityResult(int requestCode, int returnCode, Intent data){
-        if(requestCode == 1 && returnCode == 2){
+    public void onActivityResult(int requestCode, int returnCode, Intent data)
+    {
+        if (requestCode == 1 && returnCode == 2)
+        {
             TextView editedText = (TextView) findViewById(R.id.textView2);
-            if(data.hasExtra("returnkey")){
+            if (data.hasExtra("returnkey"))
+            {
                 String result = data.getExtras().getString("returnkey");
-                if(result != null && result.length() > 0){
+                if (result != null && result.length() > 0)
+                {
                     editedText.setText(result);
                 }
             }
         }
     }
 
-    public static class EditHeaderActivity extends AppCompatActivity {
+    public static class EditHeaderActivity extends AppCompatActivity
+    {
 
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState)
+        {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_edit_header);
             Bundle extras = getIntent().getExtras();
@@ -48,7 +56,9 @@ public class EditResumeActivity extends Activity {
             EditText text = (EditText) findViewById(R.id.headEditText);
             text.setText(value);
         }
-        public void onClick(View view){
+
+        public void onClick(View view)
+        {
             Intent i2 = new Intent();
             EditText text = (EditText) findViewById(R.id.headEditText);
             String back = text.getText().toString();
