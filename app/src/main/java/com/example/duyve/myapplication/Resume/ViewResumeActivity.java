147ce91a629a19b18bd5c7b1dda35836585d66be
ethwillz <ewwilliams6@gmail.com>
2016-04-12 16:55:41 -0500

    Added share button in app bar

--- app/src/main/java/com/example/duyve/myapplication/Resume/ViewResumeActivity.java
+++ app/src/main/java/com/example/duyve/myapplication/Resume/ViewResumeActivity.java
@@ -3,6 +3,8 @@ package com.example.duyve.myapplication.Resume;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -25,13 +27,18 @@ public class ViewResumeActivity extends AppCompatActivity{
 
     }
 
-    public void onShareClick(View v) {
-        if (v.getId()==R.id.ViewResumeButton) {
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.share, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
             Intent intent = new Intent(Intent.ACTION_SEND);
             intent.setType("text/plain");
             intent.putExtra(Intent.EXTRA_TEXT, "https://sizzling-torch-8367.firebaseapp.com?username=" + id);
             intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Check out my Resume!");
             startActivity(Intent.createChooser(intent, "Share"));
-        }
+            return true;
     }
 }
