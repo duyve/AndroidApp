23ca545eacbc946af432cebe7841ea8b5748d732
ethwillz <ewwilliams6@gmail.com>
2016-04-12 17:46:29 -0500

    added styling

--- app/src/main/java/com/example/duyve/myapplication/Settings/AboutActivity.java
+++ app/src/main/java/com/example/duyve/myapplication/Settings/AboutActivity.java
@@ -2,6 +2,7 @@ package com.example.duyve.myapplication.Settings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.example.duyve.myapplication.R;
 
